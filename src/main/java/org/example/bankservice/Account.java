14c8eb63d741d50bb795a8e09ece77dfe0c78d5d
Carsten Kremser <github@carstenkremser.de>
2024-03-20 16:24:33 +0100

    Exercise BankService Setup - new package bankservice, new record Client, new class Account

---
+++ src/main/java/org/example/bankservice/Account.java
@@ -0,0 +1,56 @@
+package org.example.bankservice;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Account {
+
+
+    String accountNumber;
+    BigDecimal accountBalance;
+    Client customer;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public BigDecimal getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(BigDecimal accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public Client getCustomer() {
+        return customer;
+    }
+
+    public Account(String accountNumber, Client customer) {
+        this.accountNumber = accountNumber;
+        this.customer = customer;
+        this.accountBalance = new BigDecimal("0").setScale(2);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Account account = (Account) object;
+        return Objects.equals(accountNumber, account.accountNumber) && Objects.equals(customer, account.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, customer);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", accountBalance=" + accountBalance +
+                ", customer=" + customer +
+                '}';
+    }
+}
